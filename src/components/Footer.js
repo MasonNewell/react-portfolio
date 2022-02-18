a0459a84e8bdf43ce174648a3de333e94a75e658
Mason Newell <mason.newell13@gmail.com>
2022-02-18 15:07:33 -0800

    added icons with hover

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,11 +1,24 @@
 import React from "react";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faLinkedin, faGithub } from "@fortawesome/free-brands-svg-icons";
+import { FaGithub, FaLinkedin } from "react-icons/fa";
+
 export default function Footer() {
   return (
     <div>
-      <FontAwesomeIcon icon={faLinkedin} />
-      <FontAwesomeIcon icon={faGithub} />
+      <a href="https://github.com/MasonNewell" target="_blank">
+        <FaGithub
+          size="2.5rem"
+          color="black"
+          onMouseOver={({ target }) => (target.style.color = "gray")}
+          onMouseOut={({ target }) => (target.style.color = "black")}
+        />
+      </a>
+      <a href="https://www.linkedin.com/in/mason-newell-a730ab21b/" target="_blank">
+        <FaLinkedin
+          size="2.5rem"
+          onMouseOver={({ target }) => (target.style.color = "rgb(40, 140, 178)")}
+          onMouseOut={({ target }) => (target.style.color = "rgb(40, 103, 178)")}
+        />
+      </a>
     </div>
   );
 }
