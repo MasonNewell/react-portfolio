6ff197368f1394fb1ba68ab067e0a3eba512a820
Mason Newell <mason.newell13@gmail.com>
2022-02-17 17:08:47 -0800

    added portfolio projects

--- src/components/Project.js
+++ src/components/Project.js
@@ -7,44 +7,52 @@ import workoutTracker from "../images/Projects/workoutTracker.png";
 function Project() {
   return (
     <Container>
-      <Row>
+      <Row className="align-items-stretch">
         {/* RoomFinder */}
-        <Col xs={12} md={6}>
+        <Col xs={12} md={6} className="p-4">
           <Card className="bg-dark text-white">
             <Card.Img src={roomf} alt="Card image" />
             <Card.ImgOverlay>
-              <Card.Title>RoomFinders</Card.Title>
-              <Card.Text>Uses Roomster API to find rental properties</Card.Text>
+              <Container className="bg-dark">
+                <Card.Title>RoomFinders</Card.Title>
+                <Card.Text>Uses Roomster API to find rental properties</Card.Text>
+              </Container>
             </Card.ImgOverlay>
           </Card>
         </Col>
         {/* Budget Tracker */}
-        <Col xs={12} md={6}>
-          <Card className="bg-dark text-black">
+        <Col xs={12} md={6} className="p-4">
+          <Card className="bg-dark text-white">
             <Card.Img src={budgetTracker} alt="Card image" />
             <Card.ImgOverlay>
-              <Card.Title>Budget Tracker</Card.Title>
-              <Card.Text>MongoDB/Mongoose/IndexedDB to keep track of budget info</Card.Text>
+              <Container className="bg-dark">
+                <Card.Title>Budget Tracker</Card.Title>
+                <Card.Text>MongoDB/Mongoose/IndexedDB to keep track of budget info</Card.Text>
+              </Container>
             </Card.ImgOverlay>
           </Card>
         </Col>
         {/* Techblog */}
-        <Col xs={12} md={6}>
-          <Card className="bg-dark text-black">
+        <Col xs={12} md={6} className="p-4">
+          <Card className="bg-dark text-white">
             <Card.Img src={techBlog} alt="Card image" />
             <Card.ImgOverlay>
-              <Card.Title>Techblog</Card.Title>
-              <Card.Text>Node/Express/MySQL</Card.Text>
+              <Container className="bg-dark">
+                <Card.Title>Techblog</Card.Title>
+                <Card.Text>Node/Express/MySQL</Card.Text>
+              </Container>
             </Card.ImgOverlay>
           </Card>
         </Col>
         {/* Workout Tracker */}
-        <Col xs={12} md={6}>
-          <Card className="bg-dark text-black">
+        <Col xs={12} md={6} className="p-4">
+          <Card className="bg-dark text-white">
             <Card.Img src={workoutTracker} alt="Card image" />
             <Card.ImgOverlay>
-              <Card.Title>Workout Tracker</Card.Title>
-              <Card.Text>Node/Express/NoSQL/MongoDB</Card.Text>
+              <Container className="bg-dark">
+                <Card.Title>Workout Tracker</Card.Title>
+                <Card.Text>Node/Express/NoSQL/MongoDB</Card.Text>
+              </Container>
             </Card.ImgOverlay>
           </Card>
         </Col>
