7c4b490025e0423bb4a1e419e8c0ea93d7f28009
Mason Newell <mason.newell13@gmail.com>
2022-02-20 16:20:19 -0800

    fixed sizing, margin

--- src/components/Project.js
+++ src/components/Project.js
@@ -6,14 +6,14 @@ import techBlog from "../images/Projects/techBlog.png";
 import workoutTracker from "../images/Projects/workoutTracker.png";
 function Project() {
   return (
-    <Container>
+    <Container className="my-4">
       <Row>
         {/* RoomFinder */}
         <Col xs={12} md={6} className="p-4">
-          <Card className="bg-dark text-white">
+          <Card className="h-100">
             <Card.Img src={roomf} alt="Card image" />
             <Card.ImgOverlay>
-              <Container className="bg-dark">
+              <Container className="bg-dark text-white">
                 <Card.Title>RoomFinders</Card.Title>
                 <Card.Text>Uses Roomster API to find rental properties</Card.Text>
               </Container>
@@ -22,10 +22,10 @@ function Project() {
         </Col>
         {/* Budget Tracker */}
         <Col xs={12} md={6} className="p-4">
-          <Card className="bg-dark text-white">
+          <Card className="h-100">
             <Card.Img src={budgetTracker} alt="Card image" />
             <Card.ImgOverlay>
-              <Container className="bg-dark">
+              <Container className="bg-dark text-white">
                 <Card.Title>Budget Tracker</Card.Title>
                 <Card.Text>MongoDB/Mongoose/IndexedDB to keep track of budget info</Card.Text>
               </Container>
@@ -34,10 +34,10 @@ function Project() {
         </Col>
         {/* Techblog */}
         <Col xs={12} md={6} className="p-4">
-          <Card className="bg-dark text-white">
+          <Card className="h-100">
             <Card.Img src={techBlog} alt="Card image" />
             <Card.ImgOverlay>
-              <Container className="bg-dark">
+              <Container className="bg-dark text-white">
                 <Card.Title>Techblog</Card.Title>
                 <Card.Text>Node/Express/MySQL</Card.Text>
               </Container>
@@ -46,10 +46,10 @@ function Project() {
         </Col>
         {/* Workout Tracker */}
         <Col xs={12} md={6} className="p-4">
-          <Card className="bg-dark text-white">
+          <Card className="h-100">
             <Card.Img src={workoutTracker} alt="Card image" />
             <Card.ImgOverlay>
-              <Container className="bg-dark">
+              <Container className="bg-dark text-white">
                 <Card.Title>Workout Tracker</Card.Title>
                 <Card.Text>Node/Express/NoSQL/MongoDB</Card.Text>
               </Container>
